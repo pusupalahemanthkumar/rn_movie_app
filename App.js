08650becd412d4ed8947156e110fc058291c837e
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2022-03-03 17:49:13 +0530

    added bottom navigation

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import { Feather } from "@expo/vector-icons";
+// <Feather name="home" size={24} color="black" />
+
+// <Feather name="search" size={24} color="black" />
+
+// <Feather name="bookmark" size={24} color="black" />
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import SavedScreeen from "./screens/SavedScreeen";
+import SearchScreen from "./screens/SearchScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+
+       screenOptions={
+        {
+          tabBarActiveTintColor:'red',
+          tabBarInactiveTintColor:'grey',
+          headerShown:false,
+        }
+       }
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => <Feather name="home" size={24} color="black" />,
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{
+            tabBarIcon: () => <Feather name="search" size={24} color="black" />,
+          }}
+        />
+        <Tab.Screen
+          name="Saved"
+          component={SavedScreeen}
+          options={{
+            tabBarIcon: () => (
+              <Feather name="bookmark" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/SavedScreeen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SavedScreeen = () => {
+  return (
+    <View style={{ flex:1, justifyContent:"center", alignItems:'center'}}>
+      <Text>SavedScreeen</Text>
+    </View>
+  )
+}
+
+export default SavedScreeen
+
+const styles = StyleSheet.create({})
