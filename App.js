9868af46f46beda564e25ac7c1d0740c904a14da
Sakshi Sharma <rmanshotra@gmail.com>
2022-03-08 16:03:52 +0530

    folder name change

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MainNavigator from './Navigation/MainNavigator';
+import MainNavigator from './navigation/MainNavigator';
 
 export default function App() {
   return (
